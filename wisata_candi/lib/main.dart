89754ae0ab2b3bd1233774bd238eca3517bef11e
MichaelRichi <michaelrichi_2226240154@mhs.mdp.ac.id>
2024-11-10 22:42:29 +0700

    ADD : dart.tool, main.dart
    Menambahkan folder dart.tool dan isi main.dart

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -2,19 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
-};
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Wisata Candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple), // Untuk Tema Warna
+        useMaterial3: true,
       ),
+      home: DetailScreen(),
     );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
